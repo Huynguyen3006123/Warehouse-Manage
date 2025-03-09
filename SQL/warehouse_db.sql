66f3862ba934cc7b818a623bbb857707448e4d3a
Nguyễn Tấn Phát <147572867+Hckr2005@users.noreply.github.com>
2025-03-09 10:40:43 -0700

    Add files via upload

---
+++ SQL/warehouse_db.sql
@@ -0,0 +1,299 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th3 09, 2025 lúc 06:39 PM
+-- Phiên bản máy phục vụ: 10.4.32-MariaDB
+-- Phiên bản PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `warehouse_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `danhmuc`
+--
+
+CREATE TABLE `danhmuc` (
+  `MaDanhMuc` varchar(8) NOT NULL,
+  `TenDanhMuc` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `MoTa` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `danhmuc`
+--
+
+INSERT INTO `danhmuc` (`MaDanhMuc`, `TenDanhMuc`, `MoTa`) VALUES
+('1', 'Ao', 'Dai'),
+('2', 'Quan', 'Ngan');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `hoadon`
+--
+
+CREATE TABLE `hoadon` (
+  `MaDon` varchar(8) NOT NULL,
+  `MaKH` varchar(8) NOT NULL,
+  `NgayDat` datetime NOT NULL DEFAULT current_timestamp(),
+  `MaSP` varchar(8) NOT NULL,
+  `SoLuong` int(11) NOT NULL CHECK (`SoLuong` > 0),
+  `DonGia` decimal(18,4) NOT NULL CHECK (`DonGia` > 0),
+  `ThanhTien` decimal(18,4) GENERATED ALWAYS AS (`SoLuong` * `DonGia`) STORED,
+  `TrangThai` enum('Đang xử lý','Đã giao','Đã hủy') NOT NULL DEFAULT 'Đang xử lý',
+  `GhiChu` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `hoadon`
+--
+
+INSERT INTO `hoadon` (`MaDon`, `MaKH`, `NgayDat`, `MaSP`, `SoLuong`, `DonGia`, `TrangThai`, `GhiChu`) VALUES
+('HD1', 'KH1', '2025-02-19 00:00:00', '1', 5, 50000.0000, 'Đã giao', 'Giao hàng thành công');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `khachhang`
+--
+
+CREATE TABLE `khachhang` (
+  `MaKH` varchar(8) NOT NULL,
+  `TenKH` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `DiaChi` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `SoDienThoai` varchar(20) NOT NULL,
+  `Email` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `khachhang`
+--
+
+INSERT INTO `khachhang` (`MaKH`, `TenKH`, `DiaChi`, `SoDienThoai`, `Email`) VALUES
+('KH1', 'Nguyen Van A', '123 Le Loi', '0987654321', 'a@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `nguoidung`
+--
+
+CREATE TABLE `nguoidung` (
+  `UserID` varchar(8) NOT NULL,
+  `UserName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `Role` enum('Admin','Employee') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `nguoidung`
+--
+
+INSERT INTO `nguoidung` (`UserID`, `UserName`, `Password`, `Role`) VALUES
+('AD', 'ad', '$2y$10$B1gLVNLL40GtY5qc2r.bwOV4l5S4rpkzwqL/d2GVwFe/Ni1sym/FW', 'Admin'),
+('U1', 'u1', '$2y$10$OZYWcMlhqappjUBo8L/RgORpe29gWNEEteiESllCRqdcsxSpu6frC', 'Employee');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `nhacungcap`
+--
+
+CREATE TABLE `nhacungcap` (
+  `MaNCC` varchar(8) NOT NULL,
+  `TenNCC` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `DiaChi` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `SoDienThoai` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `nhacungcap`
+--
+
+INSERT INTO `nhacungcap` (`MaNCC`, `TenNCC`, `DiaChi`, `SoDienThoai`) VALUES
+('1', 'NCC1', 'DiaChi1', '123456789');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `nhap`
+--
+
+CREATE TABLE `nhap` (
+  `MaNhap` varchar(8) NOT NULL,
+  `MaSP` varchar(8) NOT NULL,
+  `SoLuong` int(11) NOT NULL CHECK (`SoLuong` > 0),
+  `GiaNhap` decimal(18,4) NOT NULL CHECK (`GiaNhap` >= 0),
+  `NgayNhap` datetime NOT NULL DEFAULT current_timestamp(),
+  `MaNCC` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `nhap`
+--
+
+INSERT INTO `nhap` (`MaNhap`, `MaSP`, `SoLuong`, `GiaNhap`, `NgayNhap`, `MaNCC`) VALUES
+('1', '1', 10, 100000.0000, '2021-01-01 00:00:00', '1'),
+('2', '2', 20, 50000.0000, '2021-01-02 00:00:00', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `sanpham`
+--
+
+CREATE TABLE `sanpham` (
+  `MaSP` varchar(8) NOT NULL,
+  `TenSP` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `MaDanhMuc` varchar(8) NOT NULL,
+  `DVT` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `SoLuong` int(11) NOT NULL DEFAULT 0 CHECK (`SoLuong` >= 0),
+  `GiaNhap` decimal(18,4) NOT NULL CHECK (`GiaNhap` >= 0),
+  `GiaBanLe` decimal(18,4) NOT NULL CHECK (`GiaBanLe` >= 0),
+  `GiaBanSi` decimal(18,4) NOT NULL CHECK (`GiaBanSi` >= 0),
+  `MoTa` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `MaNCC` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `sanpham`
+--
+
+INSERT INTO `sanpham` (`MaSP`, `TenSP`, `MaDanhMuc`, `DVT`, `SoLuong`, `GiaNhap`, `GiaBanLe`, `GiaBanSi`, `MoTa`, `MaNCC`) VALUES
+('1', 'Ao ngan', '1', 'Cai', 5, 100000.0000, 150000.0000, 120000.0000, 'Ao dai', '1'),
+('2', 'Quan dai', '2', 'Cai', 20, 50000.0000, 70000.0000, 60000.0000, 'Quan ngan', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `xuat`
+--
+
+CREATE TABLE `xuat` (
+  `MaXuat` varchar(8) NOT NULL,
+  `MaSP` varchar(8) NOT NULL,
+  `SoLuong` int(11) NOT NULL CHECK (`SoLuong` > 0),
+  `GiaXuat` decimal(18,4) NOT NULL,
+  `NgayXuat` datetime NOT NULL DEFAULT current_timestamp(),
+  `MaKH` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `xuat`
+--
+
+INSERT INTO `xuat` (`MaXuat`, `MaSP`, `SoLuong`, `GiaXuat`, `NgayXuat`, `MaKH`) VALUES
+('X1', '1', 5, 50000.0000, '2025-02-19 00:00:00', 'KH1');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `danhmuc`
+--
+ALTER TABLE `danhmuc`
+  ADD PRIMARY KEY (`MaDanhMuc`);
+
+--
+-- Chỉ mục cho bảng `hoadon`
+--
+ALTER TABLE `hoadon`
+  ADD PRIMARY KEY (`MaDon`),
+  ADD KEY `MaKH` (`MaKH`),
+  ADD KEY `MaSP` (`MaSP`);
+
+--
+-- Chỉ mục cho bảng `khachhang`
+--
+ALTER TABLE `khachhang`
+  ADD PRIMARY KEY (`MaKH`);
+
+--
+-- Chỉ mục cho bảng `nguoidung`
+--
+ALTER TABLE `nguoidung`
+  ADD PRIMARY KEY (`UserID`),
+  ADD UNIQUE KEY `UserName` (`UserName`);
+
+--
+-- Chỉ mục cho bảng `nhacungcap`
+--
+ALTER TABLE `nhacungcap`
+  ADD PRIMARY KEY (`MaNCC`);
+
+--
+-- Chỉ mục cho bảng `nhap`
+--
+ALTER TABLE `nhap`
+  ADD PRIMARY KEY (`MaNhap`),
+  ADD KEY `MaSP` (`MaSP`),
+  ADD KEY `MaNCC` (`MaNCC`);
+
+--
+-- Chỉ mục cho bảng `sanpham`
+--
+ALTER TABLE `sanpham`
+  ADD PRIMARY KEY (`MaSP`),
+  ADD KEY `MaDanhMuc` (`MaDanhMuc`),
+  ADD KEY `MaNCC` (`MaNCC`);
+
+--
+-- Chỉ mục cho bảng `xuat`
+--
+ALTER TABLE `xuat`
+  ADD PRIMARY KEY (`MaXuat`),
+  ADD KEY `MaSP` (`MaSP`),
+  ADD KEY `MaKH` (`MaKH`);
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `hoadon`
+--
+ALTER TABLE `hoadon`
+  ADD CONSTRAINT `HoaDon_1` FOREIGN KEY (`MaKH`) REFERENCES `khachhang` (`MaKH`),
+  ADD CONSTRAINT `HoaDon_2` FOREIGN KEY (`MaSP`) REFERENCES `sanpham` (`MaSP`);
+
+--
+-- Các ràng buộc cho bảng `nhap`
+--
+ALTER TABLE `nhap`
+  ADD CONSTRAINT `Nhap_1` FOREIGN KEY (`MaSP`) REFERENCES `sanpham` (`MaSP`),
+  ADD CONSTRAINT `Nhap_2` FOREIGN KEY (`MaNCC`) REFERENCES `nhacungcap` (`MaNCC`);
+
+--
+-- Các ràng buộc cho bảng `sanpham`
+--
+ALTER TABLE `sanpham`
+  ADD CONSTRAINT `SanPham_1` FOREIGN KEY (`MaDanhMuc`) REFERENCES `danhmuc` (`MaDanhMuc`),
+  ADD CONSTRAINT `SanPham_2` FOREIGN KEY (`MaNCC`) REFERENCES `nhacungcap` (`MaNCC`);
+
+--
+-- Các ràng buộc cho bảng `xuat`
+--
+ALTER TABLE `xuat`
+  ADD CONSTRAINT `Xuat_1` FOREIGN KEY (`MaSP`) REFERENCES `sanpham` (`MaSP`),
+  ADD CONSTRAINT `Xuat_2` FOREIGN KEY (`MaKH`) REFERENCES `khachhang` (`MaKH`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
